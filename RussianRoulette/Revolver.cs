3fffa10f688e81e653338ddf83fb761374ddbb1d
Ethan-C11 <ethan.collignon.1102@gmail.com>
2024-11-19 12:59:50 +0100

    first commit

---
+++ RussianRoulette/Revolver.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RussianRoulette
+{
+    public class Revolver
+    {
+        int barrelSize { get; set; } = 6;
+        int numberOfBlanksBullets { get; set; } = 5;
+        int numberOfRealBullets { get; set; } = 1;
+        List<bool> barrelOrder { get; } = new List<bool>();
+        Random random = new Random();
+
+        public Revolver(int newBarrelSize) 
+        {
+           barrelSize = newBarrelSize;
+           numberOfRealBullets = random.Next(1, barrelSize);
+           numberOfBlanksBullets = barrelSize - numberOfRealBullets;
+           barrelOrder = generateBarrelOrder(barrelSize, numberOfRealBullets, numberOfBlanksBullets);
+           
+        }
+
+        public List<bool> generateBarrelOrder(int barrelSize, int numberOfBlanks, int numberOfReal)
+        {
+            List<bool> tempBarrelOrder = new List<bool>();
+            for (int i = 0; i < numberOfBlanks; i++)
+                tempBarrelOrder.Add(false);
+            
+
+            for (int i = 0; i < numberOfReal; i++)
+                tempBarrelOrder.Add(true);
+
+            return ShuffleBarrelOrder(tempBarrelOrder);
+
+        }
+
+        public List<bool> ShuffleBarrelOrder(List<bool> barrelOrder)
+        {
+            List<bool> newBarrelOrder = new List<bool>();
+
+            while (barrelOrder.Count > 0) { }
+            {
+                int IndexOfBulletToTake = random.Next(0, barrelOrder.Count - 1);
+                newBarrelOrder.Add(barrelOrder[IndexOfBulletToTake]);
+                barrelOrder.RemoveAt(IndexOfBulletToTake);
+            }
+
+            return newBarrelOrder;
+        }
+
+        public bool Shoot()
+        {
+            bool isBulletReal = barrelOrder.TakeLast(1).FirstOrDefault();
+            barrelOrder.RemoveAt(barrelOrder.Count-1);
+
+
+        }
+
+    }
+}
